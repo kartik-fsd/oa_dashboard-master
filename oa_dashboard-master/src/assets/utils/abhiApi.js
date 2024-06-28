b6557aeece52f1f553a993e52b11c94b49ea6ee5
Kartik Shettar <131876685+kartik-fsd@users.noreply.github.com>
2024-06-28 16:38:17 +0530

    Update abhiApi.js url (asmCardData)

--- oa_dashboard-master/src/assets/utils/abhiApi.js
+++ oa_dashboard-master/src/assets/utils/abhiApi.js
@@ -1,5 +1,6 @@
 const moveToHotLead = "https://Crmbackend.onxtasks.com/hotlead/moveToHotLead";
-const asmCardData = "https://Crmbackend.onxtasks.com/agreement/overview";
+//const asmCardData = "https://Crmbackend.onxtasks.com/agreement/overview";
+const asmCardData = "https://spappbackend.onxtasks.com/agreement/overview";
 const getcurrentDate = "https://Crmbackend.onxtasks.com/test/getcurrenttime";
 const sendWhatsappForSp =
   "https://appbackend.onxtasks.com/onboard-user/sendSpMessage";
